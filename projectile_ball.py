fe90385df00f4a89e667971733be8fd561218060
nripesh <nripesh@baylabs.io>
2020-11-29 12:24:18 -0800

    geometry added

--- projectile_ball.py
+++ projectile_ball.py
@@ -1,26 +1,57 @@
-from tkinter import *
+import math
 import time
 
+from tkinter import (
+    Canvas,
+    Tk,
+)
+
+# ball
 WIDTH = 800
 HEIGHT = 500
+BALL_POS = (30, 430)
 SIZE = 50
-tk = Tk()
-canvas = Canvas(tk, width=WIDTH, height=HEIGHT, bg="grey")
-canvas.pack()
-color = 'black'
+BALL_COLOR = 'black'
+
+# ball starting pad
+START_PAD_POS = (20, HEIGHT - 20)
+START_PAD_SIZE = (70, 10)
+
+# basket
+RING_POSITION = (650, 200)
+RING_SIZE = 100
+POLE_POSITION = (
+    RING_POSITION[0] + RING_SIZE / 2 - 8,
+    RING_POSITION[1] + RING_SIZE
+)
+POLE_SIZE = (16, 200)
 
 
 class Ball:
-    def __init__(self):
-        self.shape = canvas.create_oval(0, 0, SIZE, SIZE, fill=color)
-        self.speedx = 9 # changed from 3 to 9
-        self.speedy = 9 # changed from 3 to 9
+    def __init__(self, root, canvas):
+        self.root = root
+        self.canvas = canvas
+        self.shape = self.canvas.create_oval(
+            BALL_POS[0], BALL_POS[1],
+            BALL_POS[0] + SIZE,
+            BALL_POS[1] + SIZE, fill=BALL_COLOR
+        )
+        self.pad = canvas.create_rectangle(
+            START_PAD_POS[0], START_PAD_POS[1],
+            START_PAD_POS[0] + START_PAD_SIZE[0],
+            START_PAD_POS[1] + START_PAD_SIZE[1],
+        )
+        self.speedx = 9  # changed from 3 to 9
+        self.speedy = 9  # changed from 3 to 9
+        self.active = False
+
+    def sim_start(self, event):
         self.active = True
         self.move_active()
 
     def ball_update(self):
-        canvas.move(self.shape, self.speedx, self.speedy)
-        pos = canvas.coords(self.shape)
+        self.canvas.move(self.shape, self.speedx, self.speedy)
+        pos = self.canvas.coords(self.shape)
         if pos[2] >= WIDTH or pos[0] <= 0:
             self.speedx *= -1
         if pos[3] >= HEIGHT or pos[1] <= 0:
@@ -29,7 +60,37 @@ class Ball:
     def move_active(self):
         if self.active:
             self.ball_update()
-            tk.after(40, self.move_active) # changed from 10ms to 30ms
+            self.root.after(40, self.move_active) # changed from 10ms to 30ms
 
-ball = Ball()
-tk.mainloop()
+
+class Basket:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.ring = self.canvas.create_oval(
+            RING_POSITION[0], RING_POSITION[1],
+            RING_POSITION[0] + RING_SIZE,
+            RING_POSITION[1] + RING_SIZE, outline='blue'
+        )
+        self.pole = self.canvas.create_rectangle(
+            POLE_POSITION[0], POLE_POSITION[1],
+            POLE_POSITION[0] + POLE_SIZE[0],
+            POLE_POSITION[1] + POLE_SIZE[1], outline='blue'
+        )
+
+
+class Simulation:
+    def __init__(self):
+        self.root = Tk()
+        self.canvas = Canvas(self.root, width=WIDTH, height=HEIGHT, bg="grey")
+        self.canvas.pack()
+
+        self.ball = Ball(self.root, self.canvas)
+        self.basket = Basket(self.canvas)
+
+        print('gogo')
+        self.root.bind("<space>", self.ball.sim_start)
+
+        self.root.mainloop()
+
+
+sim = Simulation()
